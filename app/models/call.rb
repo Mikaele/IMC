b19d0287c76916a2dc36e8cf2ba69cc1ea18aad9
mikaele <mikaele164@gmail.com>
2014-05-26 22:24:00 -0300

    validação não terminei

--- app/models/call.rb
+++ app/models/call.rb
@@ -2,4 +2,7 @@ class Call < ActiveRecord::Base
   belongs_to :technic
   belongs_to :client
   has_and_belongs_to_many :services
+
+  validates_presence_of :client_id,:service_ids
+  validates_associated :technic,:client,:services
 end
--- app/models/client.rb
+++ app/models/client.rb
@@ -1,3 +1,5 @@
 class Client < ActiveRecord::Base
   belongs_to :user
+  #validates_presence_of :user
+ # validates_associated :user
 end
