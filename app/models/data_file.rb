90039ab5c3f581ab8161fc3ab17ba7ff2f0fa507
mikaele <mikaele164@gmail.com>
2014-06-25 19:28:04 -0300

    exibindo imagens

--- app/models/data_file.rb
+++ app/models/data_file.rb
@@ -1,13 +1,15 @@
 class DataFile < ActiveRecord::Base
   def self.save(upload,folder,id,named)
-    name =  upload["#{named}"].original_filename
-    directory = "public/#{folder}/#{id.to_s}"
-    unless File.exists?(directory)
-      FileUtils.mkdir(directory)
+    if upload["#{named}"]
+      name =  upload["#{named}"].original_filename
+      directory = "public/#{folder}/#{id.to_s}"
+      unless File.exists?(directory)
+        FileUtils.mkdir(directory)
+      end
+      # create the file path
+      path = File.join(directory, name)
+      # write the file
+      File.open(path, "wb") { |f| f.write(upload["#{named}"].read) }
     end
-    # create the file path
-    path = File.join(directory, name)
-    # write the file
-    File.open(path, "wb") { |f| f.write(upload["#{named}"].read) }
   end
 end
