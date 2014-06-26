8b6eb1cc79f69a4b2e98dc0c6fae5a29e1742124
mikaele <mikaele164@gmail.com>
2014-06-26 17:42:42 -0300

    login quando acesso negado

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   rescue_from CanCan::AccessDenied do |exception|
-    redirect_to '/index', :alert => exception.message
+    redirect_to '/index', :alert => 'Acesso negado!'
   end
   def current_ability
     @current_ability ||= ::Ability.new(current_user)
