617b8656c0bcd7d3c6b25b11ae77bc12f0428f73
mikaele <mikaele164@gmail.com>
2014-06-26 14:56:31 -0300

    show incidentes

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,7 +4,6 @@ class Ability
   def initialize(user)
     # Define abilities for the passed in user here. For example:
     # user ||= User.new # guest user (not logged in)
-    puts "=================="+user.colaborador.to_param.to_s
      if user
        if user.colaborador
          if user.colaborador.perfil.nome=='Admin'
@@ -19,23 +18,41 @@ class Ability
         elsif user.colaborador.perfil.nome=='Tec1'
          can :read, Call
          can :update, Call
-         can [:index], Ic
-         can [:index],Service
-         can :index,Colaborador
-         can :index,Client
-         can [:create,:update,:index],Solution
-         can [:create,:update,:index],Incidente
+         can [:index,:read], Ic
+         can [:index,:read],Service
+         can :read,Colaborador
+         can :read,Client
+         can [:index],Solution
+         can [:index],Incidente
 
         elsif user.colaborador.perfil.nome=='Tec2'
+          can :read, Call
+          can :update, Call
+          can [:index], Ic
+          can [:index],Service
+          can :index,Colaborador
+          can :index,Client
+          can [:create,:update,:index],Solution
+          can [:create,:update,:index],Incidente
 
         elsif user.colaborador.perfil.nome=='Tec3'
+          can :read, Call
+          can :update, Call
+          can [:manage], Ic
+          can [:manage],Service
+          can :index ,Colaborador
+          can :index,Client
+          can [:create,:update,:index],Solution
+          can [:create,:update,:index],Incidente
         end
        else
+         if user.client
         can :create,Call
         can :read,Ic
         can :read,Service
         can :create,Call
         cannot :index,Call
+        end
        end
     end
     #
