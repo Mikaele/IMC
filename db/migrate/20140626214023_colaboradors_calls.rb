fd26cfd94fb8b1d91046cd3c57b4d543b2d2140c
mikaele <mikaele164@gmail.com>
2014-06-29 09:28:31 -0300

    views->index com restrição

--- db/migrate/20140626214023_colaboradors_calls.rb
+++ db/migrate/20140626214023_colaboradors_calls.rb
@@ -2,7 +2,7 @@ class ColaboradorsCalls < ActiveRecord::Migration
   def self.up
     create_table 'colaboradors_calls' do |t|
       t.column :colaborador_id, :integer
-      t.column :service_id_id, :integer
+      t.column :call_id, :integer
       t.timestamps
 
     end
